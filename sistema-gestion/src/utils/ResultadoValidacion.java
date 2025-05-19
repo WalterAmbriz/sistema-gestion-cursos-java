c5ab44a833b4471a3f871c750fbccb808c6d916c
WalterAmbriz <walterardev@gmail.com>
2025-05-18 20:12:06 -0600

    feat: adding code of ResultadoValidacion.java in utils

--- sistema-gestion/src/utils/ResultadoValidacion.java
+++ sistema-gestion/src/utils/ResultadoValidacion.java
@@ -1,5 +1,19 @@
 package utils;
 
 public class ResultadoValidacion {
-    
+    private boolean valido;
+    private String mensaje;
+
+    public ResultadoValidacion(boolean valido, String mensaje) {
+        this.valido = valido;
+        this.mensaje = mensaje;
+    }
+
+    public boolean esValido() {
+        return valido;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
 }
