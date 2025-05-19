94c0b6b9a3d8802d14e7c07cbadc3dcaff833d36
WalterAmbriz <walterardev@gmail.com>
2025-05-19 07:40:14 -0600

    fix(model): Correcting the structure of toString() in Estudiante.java

--- sistema-gestion/src/model/Estudiante.java
+++ sistema-gestion/src/model/Estudiante.java
@@ -51,6 +51,7 @@ public class Estudiante {
                 ", nombre= " + nombre + '\'' +
                 ", nota= " + nota +
                 ", asistencia= " + asistencia +
+                ", esAprovado= " + esAprobado() +
                 '}';
     }
 
